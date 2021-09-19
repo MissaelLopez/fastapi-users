62bf3dd01f7ce40432744d49cb5447b92fd3fd14
MissaelLopez <missaelnj8@gmail.com>
2021-09-19 12:25:47 -0500

    First commit

---
+++ config/db.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine, MetaData, engine
+
+DB_CONNECTION = "postgresql"
+DB_HOST = "ec2-54-158-247-97.compute-1.amazonaws.com"
+DB_NAME = "d6gnk3ar4pba3p"
+DB_PORT = 5432
+DB_USER = "mkiatgybkpqblr"
+DB_PASSWORD = "64cb184329cfe6ce7f6c9df6b19a5667e3955fa73dfa4ae181bd0c0d328ee838"
+
+engine = create_engine(f"{DB_CONNECTION}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+
+meta = MetaData()
+
+conn = engine.connect()
